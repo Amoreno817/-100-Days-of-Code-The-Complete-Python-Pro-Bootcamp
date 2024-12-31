f4b1d7f010b12cca6d56514c3f4d50e1d3e12050
Alejandro Moreno <98673485+Amoreno817@users.noreply.github.com>
2024-12-31 12:02:40 -0500

    Add files via upload

---
+++ Day 6_EscapingTheMaze.py
@@ -0,0 +1,47 @@
+# Functions, Code Blocks & While Loops
+
+num_char = len("Hello")
+print(num_char)
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+# 1) Define the function first, then call it. Whatever is in the line of code under the 
+# function will trigger. Be sure to add the "():" and indent
+#_____________________________________________________________________________________________
+# Karel the robot
+# def turn_around():
+#     turn_left()
+#     turn_left()
+    
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    # for step in range(6):
+#     jump()
+#_________________________________________________________________________________________________
+chocolates = ['milk', 'dark', 'white']
+
+# Using 'chocolate' as the loop variable
+for chocolate in chocolates:
+    print(chocolate)
+
+# Using 'item' as the loop variable
+for item in chocolates:
+    print(item)
+
+# Using 'x' as the loop variable
+for x in chocolates:
+    print(x)
